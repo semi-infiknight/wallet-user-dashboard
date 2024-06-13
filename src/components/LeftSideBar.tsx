3147d66f6a8f03a144c072ad06287fd439d45d87
semi-infiknight <mathurshubhang2002@gmail.com>
2024-06-13 18:25:10 +0530

    sidebar-ui-fixed

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -1,15 +1,17 @@
-import { memo, useRef } from 'react';
+import { memo, useRef, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Home, LogOut, RefreshCcw, Slack, UserPlus, Zap } from 'react-feather';
+import { Home, LogOut, Slack, UserPlus, Zap } from 'react-feather';
 import ConnectWallet from './ConnectWallet';
 import ComingSoon from './ComingSoon';
 import SoulNFTComingSoon from './SoulNFTComingSoon';
 import { CONNECT_WALLET_BTN } from '../utils/Enum';
 import walletXLogo from '../assets/walletx.png';
 import souldNFTGIF from '../assets/souls/soulNFTDemo1.gif';
-import telegramIcon from '../assets/telegramIcon.svg';
-import twitterIcon from '../assets/twitterIcon.svg';
-import discordIcon from '../assets/discordIcon.svg';
+import telegramIcon from '../assets/socials/telegramIcon.svg';
+import twitterIcon from '../assets/socials/twitterIcon.svg';
+import discordIcon from '../assets/socials/discordIcon.svg';
+import mediumIcon from '../assets/socials/medium.svg';
+import soulExcite from '../assets/souls/soulExcite.gif';
 
 interface ConnectWalletType {
   disconnectWallet: () => void;
@@ -19,12 +21,6 @@ const sidebarButtons = [
   { icon: <Home />, label: 'Home', route: '/' },
   { icon: <Slack />, label: 'Rewards', route: '/rewards' },
   { icon: <Zap />, label: 'Soul NFTs', isComingSoon: true, route: null },
-  {
-    icon: <RefreshCcw />,
-    label: 'EXP to $WAX',
-    isComingSoon: true,
-    route: null,
-  },
   {
     icon: <UserPlus />,
     label: 'Refer friends',
@@ -36,20 +32,26 @@ const sidebarButtons = [
 const socialIcons = [
   {
     icon: twitterIcon,
-    alt: 'twitterIcon',
+    alt: 'twitter',
     href: 'https://twitter.com/getWalletX',
     height: 'h-6',
   },
   {
     icon: telegramIcon,
-    alt: 'telegramIcon',
+    alt: 'telegram',
     href: 'https://t.me/getwalletx',
     height: 'h-8',
   },
   {
     icon: discordIcon,
-    alt: 'discordIcon',
+    alt: 'discord',
     href: 'https://discord.com/invite/GNFPRUxxKW',
+    height: 'h-9',
+  },
+  {
+    icon: mediumIcon,
+    alt: 'medium',
+    href: 'https://getwalletx.medium.com/',
     height: 'h-9',
   },
 ];
@@ -70,20 +72,51 @@ const LeftSideBar = () => {
     }
   };
 
+  const [showExcite, setShowExcite] = useState(false);
+  const [time, setTime] = useState(Date.now());
+  const [isClickable, setIsClickable] = useState(true);
+
+  const handleClick = () => {
+    if (isClickable) {
+      setTime(Date.now());
+      setShowExcite(true);
+      setIsClickable(false); // Disable further clicks
+
+      setTimeout(() => {
+        setShowExcite(false);
+      }, 2000);
+
+      setTimeout(() => {
+        setIsClickable(true); // Re-enable clicks after 2.3 seconds
+      }, 2200);
+    }
+  };
+
   return (
     <div className="h-full flex flex-col ">
       <div className="w-full flex justify-center items-center text-white mt-2 ml-2">
-        <img className="w-80" src={walletXLogo} alt="walletXLogo" />
+        <img className="w-64" src={walletXLogo} alt="walletXLogo" />
       </div>
       <div className="relative flex-1  bg-zinc-900 border-y rounded-xl border-y-[#a66cff] px-5 mt-8">
-        <div className="mt-2">
+        <div className="mt-2 cursor-pointer">
           <SoulNFTComingSoon>
-            <img className="h-16" src={souldNFTGIF} alt="Soul NFT" />
+            <div onClick={() => handleClick()} className="relative">
+              <img className={`h-16 min-w-16 ${!isClickable ? 'opacity-60' : ''}`} src={souldNFTGIF} alt="Soul NFT" />
+              {showExcite && (
+                <img
+                  key={time}
+                  className="absolute left-0 top-0 h-16 w-16 scale-[3]"
+                  src={soulExcite}
+                  alt="Soul NFT Excite"
+                />
+              )}
+            </div>
           </SoulNFTComingSoon>
         </div>
-        <div className="flex flex-col gap-8 py-8 px-4 rounded-xl neomorphic w-full mt-5">
+
+        <div className="flex flex-col gap-6 justify-around py-8 px-4 rounded-xl neomorphic w-full mt-5">
           {sidebarButtons.map(({ icon, label, isComingSoon, route }, index) => (
-            <div key={index} className="overflow-hidden">
+            <div key={index}>
               {isComingSoon ? (
                 <ComingSoon>
                   <button className="flex items-center gap-2 text-xl text-neutral-100 opacity-20">
@@ -92,8 +125,8 @@ const LeftSideBar = () => {
                 </ComingSoon>
               ) : (
                 <button
-                  className={`flex items-center gap-3 text-2xl text-neutral-100 ${
-                    location.pathname === route ? 'opacity-100' : 'opacity-20'
+                  className={`flex items-center gap-3 text-xl text-neutral-100 ${
+                    location.pathname === route ? 'opacity-100' : 'opacity-60'
                   }`}
                   onClick={() => handleButtonClick(route)}
                 >
